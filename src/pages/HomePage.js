a3dab084e1d5023022f15470e6e1164dd14ea017
Chung Mạnh Quỳnh <manhquynh.ms@outlook.com>
2018-12-22 22:04:29 +0700

    modifed create event button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -287,6 +287,7 @@ const styles = {
     marginRight: 'auto',
     marginTop: 32,
     borderRadius: 0,
+    boxShadow: '0px 0px 12px rgba(0, 0, 0, 0.5)',
     '&:hover': {
       backgroundColor: Colors.primaryDark,
       color: Colors.white,
